efbd62432933623863186d38ea2e464d39772d71
uemoto <uemoto@re-vite.com>
2022-12-26 16:40:07 +0900

    revise4

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+npm run dev
+
 echo "Caching config..."
 php artisan config:cache
 
